e4e4464eb443ac08a36367c90db2d2839530a9f1
michael TanLiao <michael.tanliao@gmail.com>
2013-09-18 19:25:47 -0500

    deltemigration

--- db/migrate/20130712224835_remove_column_season_ii_team.rb
+++
@@ -1,9 +0,0 @@
-class RemoveColumnSeasonIiTeam < ActiveRecord::Migration
-  def up
-  	remove_column :season_teams, :season_team_user_id
-  end
-
-  def down
-  	add_column :season_teams, :season_team_user_id, :int
-  end
-end
